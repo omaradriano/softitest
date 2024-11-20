91507e516ed30517b766a4e20fa496287c131401
Omar Adrian <oadrian38@gmail.com>
2024-11-20 16:11:25 -0600

    Add styles for section_2

--- client/src/components/BodyComponents/Section_2.vue
+++ client/src/components/BodyComponents/Section_2.vue
@@ -1,16 +1,92 @@
 <script setup lang="ts">
+    import Icon from '../Items/Icon.vue';
+    import { ref } from 'vue';
+
+    let chars = ['1','2','3','4','5','6','7','8','9','00','0']
+    const items = ref([...chars])
 
 </script>
 
 <template>
-    <div class="Section-2">
+    <div class="Section-2 fRow">
+        <div class="bgNeutral calculator">
+            <div class="fRow"
+                :style="{
+                    borderBottom: '1px solid black',
+                    paddingBottom: '10px',
+                    alignItems: 'center',
+                }">
+                <input type="number"
+                    :style="{
+                        width: '100%',
+                        height: '35px',
+                        fontSize: '18px',
+                        textAlign: 'end',
+                        paddingRight: '10px',
+                        border: 'none',
+                        backgroundColor: 'transparent',
+                        outline: 'none'
+                    }">
+                <Icon icon-name="Backspace"/>
+            </div>
 
+            <div class="calc_buttons">
+                <div v-for="item in items">
+                    {{ item }}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
     .Section-2 { 
         grid-area: section_2; 
-        background-color: green;
+        padding: 20px;
+        justify-content: center;
+
+        background-color: #fff;
     }
+
+    .calculator {
+        height: 100%;
+        width: 100%;
+        border-radius: 12px;
+        max-width: 350px;
+        min-width: 300px;
+        padding: 45px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .calc_buttons {
+        display: grid;
+        grid-template-rows: repeat(4, 1fr);
+        grid-template-columns: repeat(3, 1fr);
+        gap: 8px;
+
+        width: 100%;
+        height: 80%;
+        align-items: center;
+        /* justify-content: center; */
+        justify-items: center;
+        margin-top: 12px;
+    }
+
+    .calc_buttons > div {
+        /* background-color: red; */
+        display: flex;
+        height: 100%;
+        width: 100%;
+        max-width: 64px;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        border: 1px solid #858585;
+        border-radius: 10px;
+        font-size: 28px;
+        font-weight: 500;
+    }
+
 </style>
