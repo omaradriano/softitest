605d5a6ae3ca1617c69f64ed7ef5c147d069c5f1
Omar Adrian <oadrian38@gmail.com>
2024-11-20 11:10:34 -0600

    Create HeaderBar component and add some layout to it

--- client/src/components/HeaderBar.vue
+++ client/src/components/HeaderBar.vue
@@ -1,17 +1,55 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import Icon from './Items/Icon.vue';
+
 
 </script>
 
 <template>
     <div class="header">
-
+        <div :style="{display: 'flex', alignItems: 'center', gap: '25px'}">
+            <div class='backOption'>
+                <Icon icon-name="arrow_back_ios" icon-class="txtSalmon"/>
+                <p class="txtSalmon">Atras</p>
+            </div>
+            <h2 class="txtBold">Pago de propinas</h2>
+        </div>
+        
+        <div class="SummaryContainer">
+            <p class="txtSalmon">Efectivo en caja</p>
+            <PriceSummary amount="1,500.00" txtColor="salmon" bgColor="whiteSalmon"/>
+        </div>
     </div>
 </template>
 
 <style scoped>
     .header { 
         grid-area: header; 
-        background-color: yellow;
+        background-color: #fff;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+
+        padding: 0 25px;
+        border-bottom: 1px solid black;
+    }
+
+    .backOption {
+        cursor: pointer;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        height: fit-content;
+        padding: 10px;
+        gap: 7px;
+    }
+
+    .SummaryContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 3px;
     }
 </style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -6,6 +6,8 @@ import FooterBody from './components/BodyComponents/Footer_Body.vue';
 import Section_1 from './components/BodyComponents/Section_1.vue';
 import Section_2 from './components/BodyComponents/Section_2.vue';
 import Section_3 from './components/BodyComponents/Section_3.vue';
+import Icon from './components/Items/Icon.vue';
+import PriceSummary from './components/Items/PriceSummary.vue';
 
 const app = createApp(App);
 
@@ -15,6 +17,8 @@ app
     .component('Section_1', Section_1)
     .component('Section_2', Section_2)
     .component('Section_3', Section_3)
+    .component('Icon', Icon)
+    .component('PriceSummary', PriceSummary)
 
 app.mount('#app')
 
