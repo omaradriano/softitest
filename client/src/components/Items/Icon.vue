d2bc4d217d7a54aac38a1d679a35b156fb2a5e3c
Omar Adrian <oadrian38@gmail.com>
2024-11-20 11:09:13 -0600

    Create components Icon and PriceSummary

---
+++ client/src/components/Items/Icon.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import { PropType } from 'vue';
+
+    type Color = 'black' | 'white'
+    defineProps({
+        iconName: {type: String, required: true},
+        iconClass: String,
+        iconFs: String,
+        iconClr: {type: String as PropType<Color>},
+    })
+
+</script>
+
+<template>
+    <span 
+        :class="['material-icons', iconClass]" 
+        :style="{ fontSize: iconFs, color: iconClr }">
+        {{iconName}}
+    </span>
+</template>
+
+<style scoped>
+    
+</style>
---
+++ client/src/components/Items/PriceSummary.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+    import { defineProps, PropType } from 'vue';
+    defineProps({
+        customClass: String,
+        txtColor: {
+            type: String as PropType<TxtColor>,
+            required: true
+        },
+        bgColor: {
+            type: String as PropType<BgColor>,
+            required: true
+        },
+        amount: String
+    })
+
+    type TxtColor = 'salmon' | 'dark'
+    type BgColor = 'whiteSalmon' 
+
+    let colors: Record<TxtColor, string> = {
+        salmon: 'txtSalmon',
+        dark: 'txtDark',
+    };
+    let bgs: Record<BgColor, String> = {
+        whiteSalmon: 'bgSalmon'
+    }
+
+
+</script>
+
+<template>
+    <div 
+        :class="['PriceSummary', customClass, colors[txtColor], bgs[bgColor]]">
+        <h1>${{ amount }}</h1>
+    </div>
+</template>
+
+<style scoped>
+    .PriceSummary {
+        display: block;
+        height: fit-content;
+        /* background-color: gray; */
+        width: fit-content;
+        padding: 7px 18px;
+        border-radius: 8px;
+    }
+</style>
